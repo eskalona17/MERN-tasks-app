201d53be068249e2b15d3939b3bc35b3b04ca9c3
eskalona17 <eskalona_17@hotmail.com>
2020-05-21 13:46:37 +0200

    form component for login created

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
 const Login = () => {
+    const onChange = () => {
+
+    }
     return ( 
-        <h1>Desde login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar sesión</h1>
+                <form action="">
+                    <div className="campo-form">
+                        <label htmlFor="">Email</label>
+                        <input type="email" id="email" name="email" placeholder="email" onChange={onChange}/>
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="">Password</label>
+                        <input type="password" id="password" name="password" placeholder="password" onChange={onChange}/>
+                    </div>
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Iniciar sesión"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+
      );
 }
  
