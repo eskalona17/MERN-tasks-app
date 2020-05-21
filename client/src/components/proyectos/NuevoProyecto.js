8b4543e0c26dbc5e2dcd9fc6b1a61fd1bc5ad18b
eskalona17 <eskalona_17@hotmail.com>
2020-05-21 17:00:07 +0200

    creating state for the form in the sidebar

--- client/src/components/proyectos/NuevoProyecto.js
+++ client/src/components/proyectos/NuevoProyecto.js
@@ -1,17 +1,45 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const NuevoProyecto = () => {
+
+    //state para proyecto
+    const [proyecto, guardarProyecto] = useState({
+        nombre: ''
+    })
+
+    //extraer nombre de proyecto
+    const { nombre } = proyecto;
+
+    //lee los contenidos del input
+    const onChangeProyecto = e => {
+        guardarProyecto({
+            ...proyecto,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //cuando el usuario envia un proyecto
+    const onSubmitProyecto = e => {
+        e.preventDefault();
+
+        //validar el proyecto
+
+
+        //agregar al state
+    }
   return (
     <Fragment>
       <button type="button" className="btn btn-block btn-primario">
         Nuevo Proyecto
       </button>
-      <form action="" className="formulario-nuevo-proyecto">
+      <form onSubmit={onSubmitProyecto} className="formulario-nuevo-proyecto">
         <input
           type="text"
           className="input-text"
           placeholder="Nombre Proyecto"
           name="nombre"
+          value={nombre}
+          onChange={onChangeProyecto}
         />
         <input type="submit" className="btn btn-block btn-primario" value="Agregar Proyecto"/>
       </form>
