7d32c6d624a8a8e86f88669a9db46fb4205fb641
eskalona17 <eskalona_17@hotmail.com>
2020-05-21 19:47:46 +0200

    listing tasks in the formtasks

---
+++ client/src/components/tareas/ListadoTareas.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from "react";
+import Tarea from "./Tarea";
+
+const ListadoTareas = () => {
+  const tareasProyecto = [
+    { nombre: "Elegir plataforma", estado: true },
+    { nombre: "Elegir colores", estado: false },
+    { nombre: "Elegir pacos", estado: false },
+    { nombre: "Elegir hosting", estado: true },
+  ];
+  return (
+    <Fragment>
+      <h2>Proyecto: paco</h2>
+      <ul className="listado-tareas">
+        {tareasProyecto.length === 0 ? (
+          <li className="tarea">
+            <p>No hay tareas</p>
+          </li>
+        ) : tareasProyecto.map(tarea => (
+            <Tarea tarea={tarea}/>
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
+
+export default ListadoTareas;
---
+++ client/src/components/tareas/Tarea.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tarea = () => {
+    return ( <h1>Desde tarea</h1> );
+}
+ 
+export default Tarea;
