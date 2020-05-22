f95cb6d397d489b0df9a9d1b412622dfb1a00392
eskalona17 <eskalona_17@hotmail.com>
2020-05-22 10:57:43 +0200

    starting to create the context and reducer for all the project

---
+++ client/src/context/proyectos/proyectoState.js
@@ -0,0 +1,24 @@
+import React, { useReducer } from 'react';
+
+import proyectoContext from './proyectoContext';
+import proyectoReducer from './proyectoReducer'
+
+const ProyectoState = props => {
+    const initalState = {
+        formulario : false
+    }
+
+    //dispatch para ejecutar las acciones
+    const [state, dispatch] = useReducer(proyectoReducer, initalState)
+
+    //serie de funciones para el CRUD
+    return(
+        <proyectoContext.Provider value={{
+            formulario: state.formulario
+        }}>
+            {props.children}
+        </proyectoContext.Provider>
+    )
+}
+
+export default ProyectoState
