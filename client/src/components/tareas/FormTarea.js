0eba00bf612560b0af5f3eb58c21c7e1bc75c6a4
eskalona17 <eskalona_17@hotmail.com>
2020-05-25 12:53:18 +0200

    showing tasks if there's a project selected, if not, we hide it

--- client/src/components/tareas/FormTarea.js
+++ client/src/components/tareas/FormTarea.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useContext} from "react";
+
+import proyectoContext from "../../context/proyectos/proyectoContext";
 
 const FormTarea = () => {
+
+  //extraer si un proyecto está activo
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  //si no hay proyecto seleccionado
+  if(!proyecto) return null;
+
+  //array destruncturing para extraer el proyecto actual
+  const [proyectoActual] = proyecto;
+
   return (
     <div className="formulario">
       <form action="">
