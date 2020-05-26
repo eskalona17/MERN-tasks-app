16736f34716373f2be00dc32a60e1b02eb8e75f2
eskalona17 <eskalona_17@hotmail.com>
2020-05-26 13:09:30 +0200

    creating a function to get the tasks of the project

--- client/src/components/proyectos/Proyecto.js
+++ client/src/components/proyectos/Proyecto.js
@@ -1,13 +1,25 @@
 import React, {useContext} from "react";
 import proyectoContext from '../../context/proyectos/proyectoContext'
+import tareaContext from '../../context/tareas/tareaContext'
 
 const Proyecto = ({ proyecto }) => {
 
   const proyectosContext = useContext(proyectoContext);
   const { proyectoActual } = proyectosContext;
+
+  //obtener la funcion del context de tareas
+  const tareasContext = useContext(tareaContext)
+  const {obtenerTareas} = tareasContext;
+
+  //funcion para agregar el proyecto actual
+  const seleccionarProyecto = id => {
+    proyectoActual(id); //fijar un proyecto actual
+    obtenerTareas(id); //filtrar las tareas cuando se dé click
+  }
+
   return (
     <li>
-      <button type="button" className="btn btn-blank" onClick={() => proyectoActual(proyecto.id)}>
+      <button type="button" className="btn btn-blank" onClick={() => seleccionarProyecto(proyecto.id)}>
         {proyecto.nombre}
       </button>
     </li>
