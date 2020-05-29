c74283da4fb64d4bad5363bd569e4955907a2138
eskalona17 <eskalona_17@hotmail.com>
2020-05-29 12:43:45 +0200

    setting up the routes for the users

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const conectarDB = require('./config/db')
+const express = require("express");
+const conectarDB = require("./config/db");
 
 // crear el servidor
 const app = express();
@@ -10,12 +10,15 @@ conectarDB();
 //puerto de la app
 const PORT = process.env.PORT || 4000;
 
+//importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'))
+
 //definir la pagina principal
-app.get('/', (req, res) => {
-    res.send('Hola mundo')
-})
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
 
 //arrancar la app
 app.listen(PORT, () => {
-    console.log(`El servidor está funcionando en el puerto ${PORT}`)
-})
+  console.log(`El servidor está funcionando en el puerto ${PORT}`);
+});
