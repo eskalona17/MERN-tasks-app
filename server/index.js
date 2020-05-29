1edc42aec4786973e583f407c7d524330d6d5c76
eskalona17 <eskalona_17@hotmail.com>
2020-05-29 11:16:11 +0200

    creating the server with express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log('desde index.js')
+const express = require('express');
+
+// crear el servidor
+const app = express();
+
+//puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//definir la pagina principal
+app.get('/', (req, res) => {
+    res.send('Hola mundo')
+})
+
+//arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor está funcionando en el puerto ${PORT}`)
+})
