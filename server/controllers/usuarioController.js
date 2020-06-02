ad75c30d827a4d7ee1b258c46861b6af196cab6a
eskalona17 <eskalona_17@hotmail.com>
2020-06-02 12:24:55 +0200

    creating a json web token JWT

--- server/controllers/usuarioController.js
+++ server/controllers/usuarioController.js
@@ -1,6 +1,7 @@
 const Usuario = require("../models/Usuario");
 const bcryptjs = require("bcryptjs");
 const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 exports.crearUsuario = async (req, res) => {
   //revisar si hay errores
@@ -26,8 +27,26 @@ exports.crearUsuario = async (req, res) => {
     //guardar usuario
     await usuario.save();
 
-    //mensaje de confirmacion
-    res.json({ msg: "Usuario creado correctamente" });
+    //crear y firmar el JWT
+    const payload = {
+      usuario:{
+        id: usuario.id
+      }
+    };
+
+    //firmar el JWT
+    jwt.sign(
+      payload,
+      process.env.SECRETA,
+      {
+        expiresIn: 3600, //1 hora autenticados
+      },
+      (error, token) => {
+        if (error) throw error;
+        //mensaje de confirmacion
+        res.json({ token });
+      }
+    );
   } catch (error) {
     console.log(error);
     res.status(400).send("Hubo un error");
