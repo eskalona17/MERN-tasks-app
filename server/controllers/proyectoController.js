1565aff04a3cf93a5b09690fd65c031fa90523bf
eskalona17 <eskalona_17@hotmail.com>
2020-06-04 20:12:43 +0200

    checking if the project is empty when the user inserts it in the db

--- server/controllers/proyectoController.js
+++ server/controllers/proyectoController.js
@@ -1,19 +1,25 @@
-const Proyecto = require('../models/Proyecto')
+const Proyecto = require("../models/Proyecto");
+const { validationResult } = require("express-validator");
 
-exports.crearProyecto = async(req, res) => {
-    try {
-        //crear nuevo proyecto
-        const proyecto = new Proyecto(req.body);
+exports.crearProyecto = async (req, res) => {
+  //revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
 
-        //guardar el creador via jwt
-        proyecto.creador = req.usuario.id;
+  try {
+    //crear nuevo proyecto
+    const proyecto = new Proyecto(req.body);
 
-        //guardamos el proyecto
-        proyecto.save();
-        res.json(proyecto)
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Hubo un error en el servidor')
-    }
-}
+    //guardar el creador via jwt
+    proyecto.creador = req.usuario.id;
+
+    //guardamos el proyecto
+    proyecto.save();
+    res.json(proyecto);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error en el servidor");
+  }
+};
