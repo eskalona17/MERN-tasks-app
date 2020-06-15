995f9dee0b04dae334155e1c8c1e9a6e3e0ce8d9
eskalona17 <eskalona_17@hotmail.com>
2020-06-15 12:42:50 +0200

    creating state and provider for the authentication

---
+++ client/src/context/autenticacion/authReducer.js
@@ -0,0 +1,16 @@
+import {
+  REGISTRO_EXITO,
+  REGISTRO_ERROR,
+  OBTENER_USUARIO,
+  LOGIN_EXITO,
+  LOGIN_ERROR,
+  CERRAR_SESION,
+} from "../../types/index";
+
+export default (state, action) => {
+  switch (action.type) {
+    
+    default:
+      return state;
+  }
+};
