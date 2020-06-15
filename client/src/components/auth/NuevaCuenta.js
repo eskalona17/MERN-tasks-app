7016415671a46f06949b464b0506c41dca4bc979
eskalona17 <eskalona_17@hotmail.com>
2020-06-15 16:19:14 +0200

    authenticating new user and redirect him to the dashboard

--- client/src/components/auth/NuevaCuenta.js
+++ client/src/components/auth/NuevaCuenta.js
@@ -2,14 +2,26 @@ import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import AlertaContext from "../../context/alertas/alertaContext";
 import AuthContext from "../../context/autenticacion/authContext";
+import { useEffect } from "react";
 
-const NuevaCuenta = () => {
+const NuevaCuenta = (props) => {
   //extraer los valores del context
   const alertaContext = useContext(AlertaContext);
   const { alerta, mostrarAlerta } = alertaContext;
 
   const authContext = useContext(AuthContext);
-  const { registrarUsuario } = authContext;
+  const { mensaje, autenticado, registrarUsuario } = authContext;
+
+  //en caso de que el usuario se haya autenticado o registrado o sea un registro duplicado
+  useEffect(() => {
+    if(autenticado){
+      props.history.push('/proyectos')
+    }
+    if(mensaje){
+      mostrarAlerta(mensaje.msg, mensaje.categoria)
+    }
+
+  },[mensaje, autenticado, props.history])
 
   //state para iniciar sesión
   const [usuario, guardarUsuario] = useState({
