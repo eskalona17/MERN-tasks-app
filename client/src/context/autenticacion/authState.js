c49d216d50440325ce2deb18224c5674a029a9b4
eskalona17 <eskalona_17@hotmail.com>
2020-07-23 20:02:20 +0200

    saving the authenticated user even if the page is reloaded

--- client/src/context/autenticacion/authState.js
+++ client/src/context/autenticacion/authState.js
@@ -101,6 +101,7 @@ const AuthState = (props) => {
         mensaje: state.mensaje,
         registrarUsuario,
         iniciarSesion,
+        usuarioAutenticado
       }}
     >
       {props.children}
